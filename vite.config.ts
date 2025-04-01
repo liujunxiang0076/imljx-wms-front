a06fd63a9315ac4d73e4e02fa0f0a043e8871368
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-04-01 17:19:20 +0800

    build: 更新 Vite 配置以支持自动导入和组件解析
    在 Vite 配置中添加了 `unplugin-vue-components` 和 `unplugin-auto-import` 插件，用于自动导入 Vue 组件和 API，并配置了 Ant Design Vue 的解析器。这些更改旨在简化代码中的导入语句，提高开发效率。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [AntDesignVueResolver()],
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // 已全局引入样式
+        }),
+      ],
+      dts: 'src/components.d.ts',
+    }),
+  ],
 })
