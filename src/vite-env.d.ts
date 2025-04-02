290a0c9c9f70c00895e2af9ae0cf47d973178cf8
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-04-02 09:05:55 +0800

    docs: 添加Vue组件和路径别名的类型声明
    在vite-env.d.ts文件中添加了Vue组件的类型声明，并解决了路径别名的模块声明问题，以增强类型安全性和开发体验。

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// 解决路径别名
+declare module '@/*'
