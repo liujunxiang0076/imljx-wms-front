565880ce0ecfed9a03a938dc18865e9879b09a9f
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-04-02 09:41:54 +0800

    fix: 在TypeScript声明文件中新增ASpin组件的类型定义
    在src/components.d.ts文件中，新增了对ant-design-vue的ASpin组件的类型定义，以增强项目的类型安全性和可用性。

--- src/components.d.ts
+++ src/components.d.ts
@@ -17,6 +17,7 @@ declare module 'vue' {
     AInput: typeof import('ant-design-vue/es')['Input']
     AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
     ARow: typeof import('ant-design-vue/es')['Row']
+    ASpin: typeof import('ant-design-vue/es')['Spin']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
