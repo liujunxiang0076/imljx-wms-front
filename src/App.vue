bf7f9f33fe85883089837b428e1c08314326ed4c
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-04-02 09:09:20 +0800

    style: 修改App.vue中的Login组件导入路径并将样式语言更改为SCSS
    将Login组件的导入路径从绝对路径更改为相对路径，同时将样式语言从CSS更改为SCSS，以保持项目的一致性和可维护性。

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import Login from '@/views/login/index.vue';
+import Login from './views/login/index.vue';
 </script>
 
 <template>
   <Login />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .app-container {
   width: 100%;
   height: 100%;
