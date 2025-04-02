60ba7eca44ecbd2101724436d48c45b0e6512427
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-04-02 14:29:35 +0800

    feat: 新增重定向功能组件
    在src/views/redirect/index.vue中，新增了重定向功能组件，使用Vue Router在组件挂载前根据路由参数进行页面重定向，提升了用户导航体验。

---
+++ src/views/redirect/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+onBeforeMount(() => {
+  const { params } = route;
+  const { path } = params;
+  
+  router.replace({ path: '/' + path });
+});
+</script> 
