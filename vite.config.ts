1726cc726a09cf778955c3dc7818941fb37e55f4
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-04-02 09:06:31 +0800

    build: 添加路径别名解析配置
    在vite.config.ts中添加了路径别名解析配置，将`@`映射到`src`目录，以简化模块导入路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
